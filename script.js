7536f80a756520d3a1adaddac9f5b8785183dd72
Cathy Rolfs <crolfs47@gmail.com>
2022-09-05 11:53:30 -0500

    Create functions for calculator to store operator and operand inputs and handle multiple equations

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+let num1 = '';
+let num2 = '';
+let currentOperator = null;
+let total = null;
 
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('.display');
+const buttons = document.querySelector('button');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
+let displayValue = document.querySelector('.display').textContent;
 
-
-
-
-
-
-// Basic math operator functions
+// Basic math operator functions - do we need separate functions for each operator or can I just put it in the operator function?
 function add(a, b){
     return a + b;
 }
@@ -24,15 +30,97 @@ function divide(a,b){
 
 function operate(operator, a, b) {
     if (operator === '+') {
-        return add(a, b);
+        total = add(a, b);
     }
     if (operator === '-'){
-        return subtract(a, b);
+        total = subtract(a, b);
     }
     if (operator === '*'){
-        return multiply(a, b);
+        total = multiply(a, b);
     }
     if (operator ==='/'){
-        return divide(a, b);
+        total = divide(a, b);
     }
+    console.log('total = ' + total);
+    display.textContent = total;
+    return total;
 }
+
+function setNumber(number) {
+    if (display.textContent === ''|| display.textContent === '0') {
+        display.textContent = number.textContent;
+    }
+    else if (display.textContent !== '' || display.textContent !== 0 || display.textContent !== '0') {
+        display.textContent += number.textContent;
+    }
+    displayValue = parseFloat(display.textContent);
+    console.log('displayValue = ' +displayValue);
+}    
+
+function setOperator(operator) {
+    if (display.textContent === ''){
+        return;
+    }
+    if (total === null) {
+        num1 = parseFloat(display.textContent);
+        console.log('num1 = ' + num1);
+    }
+    else {
+        num1 = parseFloat(total);
+        console.log('new num1 = ' + num1);
+    }
+    if (operator.textContent === '÷') {
+        operator.textContent = '/';
+    }
+    if (operator.textContent === 'x') {
+        operator.textContent = '*';
+    }
+    currentOperator = operator.textContent;
+    console.log('currentOperator = ' + currentOperator);
+    resetScreen();
+};
+
+function calculate() {
+    if (display.textContent === '') {
+        return;
+    }
+    num2 = parseFloat(display.textContent);
+    console.log('num2 = ' + num2);
+    display.textContent = equals.textContent;
+    resetScreen();
+    operate(currentOperator, num1, num2);
+    
+}
+
+function resetScreen() {
+    display.textContent = '';
+    displayValue = '';
+}
+
+function clearScreen() {
+    location.reload();
+}
+
+
+// Event listeners
+numbers.forEach((number) => {
+    number.addEventListener('click', () => {
+        setNumber(number);
+    })
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener('click', () => {
+        setOperator(operator);
+    }
+    )
+})
+
+equals.addEventListener('click', () => {
+    calculate();
+})
+
+clear.addEventListener('click', () => {
+    clearScreen();
+})
+
